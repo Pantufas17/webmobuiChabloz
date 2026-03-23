4e228a88d25991ae3019f8c248a5642685e2c129
Pantufas17 <nunoamarofaria@hotmail.com>
2026-03-23 14:11:07 +0100

    MAJ avec implementation avec defineModel

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref, computed} from 'vue'
+import { ref, computed } from 'vue'
+import ChampTemperature from './components/ChampTemperature.vue'
+
 
 const temperatureKelvin = ref(17)
-
 
 const tempKelvin = computed({
   get: () => temperatureKelvin.value,
@@ -18,15 +19,14 @@ const tempFahrenheit = computed({
   get: () => (temperatureKelvin.value - 273.15) * 9 / 5 + 32,
   set: (val) => { temperatureKelvin.value = (Number(val) - 32) * 5 / 9 + 273.15 }
 })
-
 </script>
 
 <template>
   <div>
     <h1>TP - Convertisseur de température</h1>
     
-    <p>Kelvin : <input type="number" v-model="tempKelvin" /></p>
-    <p>Celsius : <input type="number" v-model="tempCelsius" /></p>
-    <p>Fahrenheit : <input type="number" v-model="tempFahrenheit" /></p>
+    <ChampTemperature label="Kelvin" v-model="tempKelvin" />
+    <ChampTemperature label="Celsius" v-model="tempCelsius" />
+    <ChampTemperature label="Fahrenheit" v-model="tempFahrenheit" />
   </div>
 </template>
---
+++ src/components/ChampTemperature.vue
@@ -0,0 +1,11 @@
+<script setup>
+const model = defineModel()
+
+defineProps(['label'])
+</script>
+
+<template>
+  <p>
+    {{ label }} : <input type="number" v-model="model" />
+  </p>
+</template>
