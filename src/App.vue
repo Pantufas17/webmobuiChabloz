fcd3af2b6cf6d38ebef53c87914f7b9be73d5041
Pantufas17 <nunoamarofaria@hotmail.com>
2026-03-23 14:01:20 +0100

    Computed with the 3 temperatures mesurements Kelvin-Celcius-Farhenheit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref, computed} from 'vue'
 
 const temperatureKelvin = ref(17)
 
 
-console.log("Ma température en Kelvin est de :", temperatureKelvin.value)
+const tempKelvin = computed({
+  get: () => temperatureKelvin.value,
+  set: (val) => { temperatureKelvin.value = Number(val) }
+})
+
+const tempCelsius = computed({
+  get: () => temperatureKelvin.value - 273.15,
+  set: (val) => { temperatureKelvin.value = Number(val) + 273.15 }
+})
+
+const tempFahrenheit = computed({
+  get: () => (temperatureKelvin.value - 273.15) * 9 / 5 + 32,
+  set: (val) => { temperatureKelvin.value = (Number(val) - 32) * 5 / 9 + 273.15 }
+})
+
 </script>
 
 <template>
   <div>
-    <h1>Exercice : Convertisseur de température</h1>
+    <h1>TP - Convertisseur de température</h1>
+    
+    <p>Kelvin : <input type="number" v-model="tempKelvin" /></p>
+    <p>Celsius : <input type="number" v-model="tempCelsius" /></p>
+    <p>Fahrenheit : <input type="number" v-model="tempFahrenheit" /></p>
   </div>
 </template>
